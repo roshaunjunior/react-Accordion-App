50b49a804137f5e05f8b7525caee476e1d847f1a
Talha Roshaun <talharoshaun@gmail.com>
2022-06-24 12:39:35 +0500

    first commit

---
+++ src/Question.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const Question = (props) => {
+	const { title, info } = props
+	// add a state variable here
+    const [visibility, setVisibility] = useState (false);
+
+    
+
+	return (
+        
+		<article className="question">
+			<header>
+				<h4>{title}</h4>
+		
+				<button  className="btn" onClick = {() => setVisibility((visibility) => !visibility)}>{visibility === false ? '+' : '-'}</button>
+			</header>
+			<p>{visibility === false ? null : info}</p>
+		</article>
+	)
+}
+
+export default Question
